e5cb9cbc304b9a031638b6ad1ba602caabb0179c
melancholisch <monteirodbm@gmail.com>
2024-07-15 19:16:58 -0300

    Initial commit

---
+++ src/main/java/br/com/card_zero/CardZeroApplication.java
@@ -0,0 +1,13 @@
+package br.com.card_zero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardZeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardZeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/card_zero/CardZeroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.card_zero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardZeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
